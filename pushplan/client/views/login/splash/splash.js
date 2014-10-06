c3920c52fda6478b2c01cbb859681917a78caacb
Jeff Chin <jchin89@gmail.com>
2014-10-05 21:23:52 -0400

    added splash / not found

---
+++ pushplan/client/views/login/splash/splash.js
@@ -0,0 +1,35 @@
+
+/*****************************************************************************/
+/* Splash: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.Splash.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.Splash.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* Splash: Lifecycle Hooks */
+/*****************************************************************************/
+Template.Splash.created = function () {
+};
+
+Template.Splash.rendered = function () {
+};
+
+Template.Splash.destroyed = function () {
+};
+
+
---
+++ pushplan/start.sh
@@ -0,0 +1,35 @@
+#!bin/bash
+
+#APP_ENV="production"
+#APP_ENV="local"
+#APP_ENV="dev"
+
+printf "Start PushPlan in? (local[l], dev[d], or production[p]): "
+
+read APP_ENV
+
+#local
+if [ "$APP_ENV" = "local" ]; then
+    echo "Deploying PushPlan to localhost"
+    meteor --settings config/local/settings.json
+elif [ "$APP_ENV" = "l" ]; then
+    echo "Deploying PushPlan to localhost"
+    meteor --settings config/local/settings.json
+#dev
+elif [ "$APP_ENV" = "dev" ]; then
+    echo "Deploying PushPlan to to dev (pushplan.meteor.com)"
+    meteor deploy pushplan.meteor.com --settings config/development/settings.
+elif [ "$APP_ENV" = "dev" ]; then
+    echo "Deploying PushPlan to dev (pushplan.meteor.com)"
+    meteor deploy pushplan.meteor.com --settings config/development/settings.json
+#production
+elif [ "$APP_ENV" = "production" ]; then
+    echo "Deploying PushPlan to production (pushplan.net)"
+    meteor deploy pushplan.net --settings config/production/settings.json
+    elif [ "$APP_ENV" = "p" ]; then
+    echo "Deploying PushPlan to production (pushplan.net)"
+    meteor deploy pushplan.net --settings config/production/settings.json
+
+else
+    echo "Invalid option"
+fi
