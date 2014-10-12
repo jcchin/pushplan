c24b59f79a9f0f0408211329076171dcf653687e
Jeff Chin <jchin89@gmail.com>
2014-10-12 15:51:17 -0400

    working oauth buttons

--- pushplan/server/config.js
+++ pushplan/server/config.js
@@ -1,21 +1,20 @@
 
 ServiceConfiguration.configurations.remove({
-  service: "facebook"
-});
-ServiceConfiguration.configurations.insert({
-  service: "facebook",
-  appId: Meteor.settings.facebookClientID,
-  secret: Meteor.settings.facebookSecret
-});
+        service: "facebook"
+    });
+    ServiceConfiguration.configurations.insert({
+        service: "facebook",
+        appId: Meteor.settings.facebookClientID,
+        secret: Meteor.settings.facebookSecret
+    });
 
-
-ServiceConfiguration.configurations.remove({
-  service: "google"
-});
-ServiceConfiguration.configurations.insert({
-  service: "google",
-  clientId: Meteor.settings.googleClientID,
-  secret: Meteor.settings.googleSecret
-});
+    ServiceConfiguration.configurations.remove({
+      service: "google"
+    });
+    ServiceConfiguration.configurations.insert({
+      service: "google",
+      clientId: Meteor.settings.googleClientID,
+      secret: Meteor.settings.googleSecret
+    });
 
 
